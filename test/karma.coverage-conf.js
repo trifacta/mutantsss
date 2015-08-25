442b16902f39bc64ac363a447fbc9ca7a953c0d6
Eli Marschner <eli@trifacta.com>
2015-08-24 17:10:32 -0700

    configure ci & coverage reports

--- test/karma.coverage-conf.js
+++ test/karma.coverage-conf.js
@@ -1,7 +1,7 @@
 module.exports = function(config) {
   require('./karma.conf')({ set: function(settings) {
     settings.browserify.transform = [require('browserify-istanbul')];
-    settings.reporters.push('coverage');
+    settings.reporters.push('coverage', 'coveralls');
     settings.coverageReporter = { type: 'lcov' };
     config.set(settings);
   } });
